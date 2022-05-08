3ae0fadfd181d917d1a97587a77667728dfbfab8
meixu <songmeixu@outlook.com>
2022-05-08 21:50:41 +0800

    add a `pip` way to install gpu version of pytorch related packages

--- recipes/dcase2022_task4_baseline/conda_create_environment.sh
+++ recipes/dcase2022_task4_baseline/conda_create_environment.sh
@@ -7,6 +7,7 @@ conda install -y ffmpeg -c conda-forge
 conda install -y sox -c conda-forge
 conda install -y pandas h5py scipy
 conda install -y pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch # for gpu install (or cpu in MAC)
+pip install torch==1.10.2+cu111 torchvision==0.11.3+cu111 torchaudio==0.10.2+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 # conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
 conda install -y youtube-dl tqdm -c conda-forge
 
