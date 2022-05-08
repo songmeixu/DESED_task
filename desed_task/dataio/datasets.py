69464cc72b898ca558e17914f2f1aa6b16c4daf4
meixu <songmeixu@outlook.com>
2022-05-08 21:45:33 +0800

    skip not exists strong real audio train files

--- desed_task/dataio/datasets.py
+++ desed_task/dataio/datasets.py
@@ -20,7 +20,7 @@ def to_mono(mixture, random_ch=False):
 
 
 def pad_audio(audio, target_len, fs):
-    
+
     if audio.shape[-1] < target_len:
         audio = torch.nn.functional.pad(
             audio, (0, target_len - audio.shape[-1]), mode="constant"
@@ -28,14 +28,14 @@ def pad_audio(audio, target_len, fs):
 
         padded_indx = [target_len / len(audio)]
         onset_s = 0.000
-    
+
     elif len(audio) > target_len:
-        
+
         rand_onset = random.randint(0, len(audio) - target_len)
         audio = audio[rand_onset:rand_onset + target_len]
         onset_s = round(rand_onset / fs, 3)
 
-        padded_indx = [target_len / len(audio)] 
+        padded_indx = [target_len / len(audio)]
     else:
 
         onset_s = 0.000
@@ -45,23 +45,23 @@ def pad_audio(audio, target_len, fs):
     return audio, onset_s, offset_s, padded_indx
 
 def process_labels(df, onset, offset):
-    
-    
-    df["onset"] = df["onset"] - onset 
+
+
+    df["onset"] = df["onset"] - onset
     df["offset"] = df["offset"] - onset
-        
+
     df["onset"] = df.apply(lambda x: max(0, x["onset"]), axis=1)
     df["offset"] = df.apply(lambda x: min(10, x["offset"]), axis=1)
 
     df_new = df[(df.onset < df.offset)]
-    
+
     return df_new.drop_duplicates()
 
 
 def read_audio(file, multisrc, random_channel, pad_to):
-    
+
     mixture, fs = torchaudio.load(file)
-    
+
     if not multisrc:
         mixture = to_mono(mixture, random_channel)
 
@@ -86,7 +86,7 @@ class StronglyAnnotatedSet(Dataset):
         return_filename=False,
         random_channel=False,
         multisrc=False
-        
+
     ):
 
         self.encoder = encoder
@@ -97,9 +97,12 @@ class StronglyAnnotatedSet(Dataset):
         self.multisrc = multisrc
 
         tsv_entries = tsv_entries.dropna()
-        
+
         examples = {}
         for i, r in tsv_entries.iterrows():
+            if not os.path.exists(os.path.join(audio_folder, r["filename"])):
+                continue
+
             if r["filename"] not in examples.keys():
                 examples[r["filename"]] = {
                     "mixture": os.path.join(audio_folder, r["filename"]),
@@ -142,11 +145,11 @@ class StronglyAnnotatedSet(Dataset):
 
         # labels
         labels = c_ex["events"]
-        
+
         # to steps
         labels_df = pd.DataFrame(labels)
         labels_df = process_labels(labels_df, onset_s, offset_s)
-        
+
         # check if labels exists:
         if not len(labels_df):
             max_len_targets = self.encoder.n_frames
@@ -202,7 +205,7 @@ class WeakSet(Dataset):
         mixture, _, _, padded_indx = read_audio(
             c_ex["mixture"], self.multisrc, self.random_channel, self.pad_to
         )
-        
+
         # labels
         labels = c_ex["events"]
         # check if labels exists:
@@ -234,7 +237,7 @@ class UnlabeledSet(Dataset):
 
         self.encoder = encoder
         self.fs = fs
-        self.pad_to = pad_to * fs if pad_to is not None else None 
+        self.pad_to = pad_to * fs if pad_to is not None else None
         self.examples = glob.glob(os.path.join(unlabeled_folder, "*.wav"))
         self.return_filename = return_filename
         self.random_channel = random_channel
